41cf9da626bdcdb5e05f39db60eff3eb89b15f69
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-03-24 14:02:37 +0100

    The function get_protocol_response only returns a response instance.  Added two new functions:  - finds an id_token as the original JWS.  - gets the last authorization request

--- src/otest/check.py
+++ src/otest/check.py
@@ -6,9 +6,14 @@
     :license: APACHE 2.0, see LICENSE for more details.
 """
 import json
+
 from aatest.events import EV_PROTOCOL_RESPONSE
+from aatest.events import EV_RESPONSE
+from aatest.events import EV_REDIRECT_URL
 from future.backports.urllib.parse import parse_qs
-from oic.oauth2 import message
+
+from oic.oic import message
+from oic.oauth2 import AuthorizationRequest
 
 
 def get_provider_info(conv):
@@ -25,21 +30,43 @@ def get_protocol_response(conv, cls):
 def get_id_tokens(conv):
     res = []
     # In access token responses
-    for inst, msg in get_protocol_response(conv, message.AccessTokenResponse):
-        _dict = json.loads(msg)
-        jwt = _dict["id_token"]
-        idt = inst["id_token"]
-        res.append((idt, jwt))
+    for inst in get_protocol_response(conv, message.AccessTokenResponse):
+        res.append(inst["id_token"])
 
     # implicit, id_token in authorization response
-    for inst, msg in get_protocol_response(conv, message.AuthorizationResponse):
+    for inst in get_protocol_response(conv, message.AuthorizationResponse):
         try:
-            idt = inst["id_token"]
+            res.append(inst["id_token"])
         except KeyError:
             pass
-        else:
-            _info = parse_qs(msg)
-            jwt = _info["id_token"][0]
-            res.append((idt, jwt))
 
     return res
+
+
+def get_signed_id_tokens(conv):
+    res = []
+    for txt in conv.events.get_data(EV_RESPONSE):
+        try:
+            ent = json.loads(txt)
+        except Exception:
+            try:
+                ent = parse_qs(txt)
+            except:
+                pass
+            else:
+                try:
+                    res.append(ent['id_token'][0])
+                except KeyError:
+                    pass
+        else:
+            try:
+                res.append(ent['id_token'])
+            except KeyError:
+                pass
+
+    return res
+
+
+def get_authorization_request(conv, cls):
+    authz_req = conv.events.get_data(EV_REDIRECT_URL)[0].split('?')[1]
+    return cls().from_urlencoded(authz_req)
