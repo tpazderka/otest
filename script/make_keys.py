202ae9f2a29fe3fb5289de21e3010c2e44d1fa5b
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-04-01 13:59:35 +0200

    Key creation program.

--- script/make_keys.py
+++ script/make_keys.py
@@ -1,8 +1,6 @@
+#!/usr/bin/env python
 from oic.utils.keyio import create_and_store_rsa_key_pair
 
-key = create_and_store_rsa_key_pair("keys/pyoidc_enc", size=2048)
-key = create_and_store_rsa_key_pair("keys/pyoidc_sig", size=2048)
-key = create_and_store_rsa_key_pair("keys/2nd_enc", size=2048)
-key = create_and_store_rsa_key_pair("keys/2nd_sig", size=2048)
-key = create_and_store_rsa_key_pair("keys/3rd_enc", size=2048)
-key = create_and_store_rsa_key_pair("keys/3rd_sig", size=2048)
+for name in ['pyoidc', '2nd', '3rd']:
+    create_and_store_rsa_key_pair("keys/{}_enc".format(name), size=2048)
+    create_and_store_rsa_key_pair("keys/{}_sig".format(name), size=2048)
