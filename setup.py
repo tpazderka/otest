5d1fc1ecc50d2348217f6baa3af78da12c74e35a
Roland Hedberg <roland.hedberg@adm.umu.se>
2016-04-03 17:49:54 +0200

    Crypto -> Cryptodome

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         "requests >= 2.0.0",
         'aatest',
         'pyYAML',
-        'pycryptodome',
+        'pycryptodomex',
         'oic',
         'pyjwkest',
         'future',
