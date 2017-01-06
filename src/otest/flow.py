f3f73b5ce5bcdc3ff1b33735f792b50f75072ba0
Roland Hedberg <roland@catalogix.se>
2017-01-06 13:18:16 +0100

    Extended the flow module.

--- src/otest/flow.py
+++ src/otest/flow.py
@@ -1,6 +1,8 @@
 import json
 import os
 import re
+from otest import Unknown
+from otest.func import factory as ofactory
 
 PAT = re.compile('\${([A-Z_0-9]*)}')
 
@@ -62,7 +64,8 @@ class Flow(object):
         :param tid: The test ID
         :return: A dictionary representation of the description
         """
-        fname = os.path.join(self.fdir, tid)
+
+        fname = os.path.join(self.fdir, tid + '.json')
         fp = open(fname, 'r')
         try:
             _info = json.load(fp)
@@ -83,8 +86,8 @@ class Flow(object):
         """
         for fn in os.listdir(self.fdir):
             if fn.endswith('.json'):
-                fn = fn[:-4]
-                yield((fn, self[fn]))
+                sfn = fn[:-5]
+                yield((sfn, self[sfn]))
 
     def keys(self):
         """
@@ -93,4 +96,93 @@ class Flow(object):
         """
         for fn in os.listdir(self.fdir):
             if fn.endswith('.json'):
-                yield(fn[:-4])
+                yield(fn[:-5])
+
+    def pick(self, key, value):
+        tids = []
+        for tid, spec in self.items():
+            try:
+                _val = spec[key]
+            except KeyError:
+                pass
+            else:
+                if value == _val:
+                    tids.append(tid)
+        return tids
+
+# ==============================================================================
+
+
+def _get_cls(name, factories, use=''):
+    if use:
+        try:
+            cls = factories[use](name)
+        except Unknown:
+            pass
+        else:
+            return cls
+
+    try:
+        cls = factories[''](name)
+    except Unknown:
+        raise Exception("Unknown Class: '{}'".format(name))
+
+    return cls
+
+
+def _get_func(dic, func_factory):
+    """
+    Convert function names into function references
+
+    :param dic: A key, value dictionary where keys are function names
+    :param func_factory: Factory function used to find functions
+    :return: A dictionary with the keys replace with references to functions
+    """
+    res = {}
+    for fname, val in dic.items():
+        func = func_factory(fname)
+        if func is None:
+            func = ofactory(fname)
+
+        if func is None:
+            raise Exception("Unknown function: '{}'".format(fname))
+        res[func] = val
+
+    return res
+
+
+class RPFlow(Flow):
+    def __init__(self, fdir, cls_factories, func_factory, use=''):
+        Flow.__init__(self, fdir)
+        self.cls_factories = cls_factories
+        self.func_factory = func_factory
+        self.use = use
+
+    def expanded_conf(self, tid):
+        """
+
+        :param test_id:
+        :return:
+        """
+        spec = self[tid]
+        seq = []
+        for oper in spec["sequence"]:
+            if isinstance(oper, dict):  # Must be only one key, value item
+                if len(oper) > 1:
+                    raise SyntaxError(tid)
+                key, val = list(oper.items())[0]
+                try:
+                    seq.append((_get_cls(key, self.cls_factories, self.use),
+                                _get_func(val, self.func_factory)))
+                except Exception:
+                    print('tid:{}'.format(tid))
+                    raise
+            else:
+                try:
+                    seq.append(_get_cls(oper, self.cls_factories, self.use))
+                except Exception:
+                    print('tid:{}'.format(tid))
+                    raise
+        spec["sequence"] = seq
+
+        return spec
