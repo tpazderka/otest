9110b84412ac7896e56a62251bbb0f4dedf71862
Roland Hedberg <roland@catalogix.se>
2017-09-26 10:20:30 +0200

    Verify that the kid dictionary is correct.

--- src/otest/aus/client.py
+++ src/otest/aus/client.py
@@ -32,6 +32,27 @@ class Factory(object):
     def __init__(self, client_cls):
         self.client_cls = client_cls
 
+    def adjust_kid(self, kidd, keyjar):
+        """
+        Verify that the set of keys assigned to different usage is still
+        around.
+        
+        :param kidd: Dictionary {usage: {key_type: kid}} 
+        :param keyjar:  A KeyJar instance
+        :return: A corrected kid dictionary
+        """
+        res = {}
+        for usage, spec in kidd.items():
+            res[usage] = {}
+            for key_type, _id in spec.items():
+                if not keyjar.get(usage, key_type, kid=_id):
+                    l = keyjar.get(usage, key_type)
+                    if l:
+                        res[usage][key_type] = l[0].kid
+                else:
+                    res[usage][key_type] = _id
+        return res
+
     def make_client(self, **kw_args):
         """
         Have to get own copy of keyjar
@@ -49,6 +70,13 @@ class Factory(object):
             c_keyjar.verify_ssl = kw_args['verify_ssl']
 
         _cli = self.client_cls(**args)
+
+        try:
+            _kid = kw_args['kid']
+        except KeyError:
+            pass
+        else:
+            kw_args['kid'] = self.adjust_kid(_kid, _cli.keyjar)
 
         c_info = {'keyjar': c_keyjar}
         for arg, val in list(kw_args.items()):
